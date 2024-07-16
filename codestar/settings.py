0c548bb902896baee732985ef6b7d4265ee85cea
Alistair Driscoll <pallythecoder@gmail.com>
2024-07-16 18:58:03 +0000

    Add summernote

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DATABASE_URL")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-alistairdris-djangoblog-mh7yds72ckq.ws-eu115.gitpod.io',
                 '.herokuapp.com',
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_summernote',
     'blog',
 ]
 
