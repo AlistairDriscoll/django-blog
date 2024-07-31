a669ffa552be404f32fd2a20e90c817f5af5153b
Alistair Driscoll <pallythecoder@gmail.com>
2024-07-31 11:58:46 +0000

    Add: Comment CRUD functionality

--- about/views.py
+++ about/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
 from .forms import CollaborateForm
+
 
 # Create your views here.
 
 def about(request):
+    """
+    Renders the About page
+    """
+
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid:
+            collaborate_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                "Collaboration request received! I endeavour to respond within 2 working days."
+            )
     
     about = About.objects.all().order_by('-updated_on').first()
     collaborate_form = CollaborateForm()
@@ -13,13 +27,4 @@ def about(request):
         request,
         "about/about.html",
         {"about": about, "collaborate_form": collaborate_form},
-    )
-
-def collaborate_form(request):
-    
-
-    return render(
-        request,
-        "about/about.html",
-        {},
     )
